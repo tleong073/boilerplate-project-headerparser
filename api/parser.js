6eb2af2075b41cffe25002fb28380ed44088c0dd
Trevor Leong <tleong073@gmail.com>
2021-06-02 12:10:53 -0400

    first commit

---
+++ api/parser.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/whoami',(req,res) => {
+    console.log( req);
+    res.json({
+        ipaddress: req.headers.ipaddress,
+        language: req.headers['accept-language'],
+        software: req.headers['user-agent']
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const parser = require('./api/parser');
 // server.js
 // where your node app starts
 
@@ -21,13 +22,11 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.use('/api',parser);
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT | 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
